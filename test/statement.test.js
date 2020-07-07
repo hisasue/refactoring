c63779eeb45a58ee91b134618b686484b2a94a73
hisasue <hisa@flatz.jp>
2020-07-07 09:27:32 +0900

    align message

--- test/statement.test.js
+++ test/statement.test.js
@@ -32,11 +32,11 @@ describe('statement', () => {
 
   it('should print a statement for multiple plays, single customer and multiple seats in plain text', () => {
     let expected = "Statement for BigCo\n" +
-      "  Hamlet: $650.00 (55 seats)\n" +
-      "  As You Like It: $580.00 (35 seats)\n" +
-      "  Othello: $500.00 (40 seats)\n" +
-      "Amount owed is $1,730.00\n" +
-      "You earned 47 credits\n";
+                   "  Hamlet: $650.00 (55 seats)\n" +
+                   "  As You Like It: $580.00 (35 seats)\n" +
+                   "  Othello: $500.00 (40 seats)\n" +
+                   "Amount owed is $1,730.00\n" +
+                   "You earned 47 credits\n";
 
     expect(statement(invoicesJson[0], playsJson)).to.equal(expected);
   });
