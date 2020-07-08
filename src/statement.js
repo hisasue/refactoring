01f120c2d321137acd241ca0711007db1566875e
hisasue <hisa@flatz.jp>
2020-07-08 10:22:01 +0900

    implement htmlStatement, renderHtml

--- src/statement.js
+++ src/statement.js
@@ -14,24 +14,31 @@ export default function statement (invoice, plays) {
     result += `You earned ${data.totalVolumeCredits} credits\n`;
     return result;
 
-    function usd(aNumber) {
-      return new Intl.NumberFormat("en-US",
-                          { style: "currency", currency: "USD",
-                            minimumFractionDigits: 2 }).format(aNumber/100);
-    }
   }
 
 }
 
 export function htmlStatement (invoice, plays) {
-  let result   = "<h1>Statement for BigCo</h1>\n" +
-                 "<table>\n" +
-                 "<tr><th>play</th><th>seats</th><th>cost</th></tr>\n" +
-                 "<tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>\n" +
-                 "<tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>\n" +
-                 "<tr><td>Othello</td><td>40</td><td>$500.00</td></tr>\n" +
-                 "</table>\n" +
-                 "<p>Amount owed is <em>$1,730.00</em></p>\n" +
-                 "<p>You earned <em>47</em> credits</p>\n";
-  return result
+  return renderHtml(createStatementData(invoice, plays))
+
+  function renderHtml(data){
+    let result   = "<h1>Statement for BigCo</h1>\n" +
+                   "<table>\n" +
+                   "<tr><th>play</th><th>seats</th><th>cost</th></tr>\n"
+
+    for (let perf of data.performances) {
+      result += `<tr><td>${perf.play.name}</td><td>${perf.audience}</td><td>${usd(perf.amount)}</td></tr>\n`;
+    }
+
+    result +=  "</table>\n" +
+               `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n` +
+               `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+    return result
+  }
+}
+
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US",
+                      { style: "currency", currency: "USD",
+                        minimumFractionDigits: 2 }).format(aNumber/100);
 }
