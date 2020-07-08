c27420477597418fab147c25e2c977fac2fe1371
hisasue <hisa@flatz.jp>
2020-07-08 09:04:23 +0900

    tweak

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -5,6 +5,7 @@ export default function createStatementData(invoice, plays) {
   result.totalAmount = totalAmount(result)
   result.totalVolumeCredits = totalVolumeCredits(result)
   return result
+  
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance)
     result.play = playFor(result)
@@ -12,7 +13,6 @@ export default function createStatementData(invoice, plays) {
     result.volumeCredits = volumeCreditsFor(result)
     return result
   }
-
 
   function amountFor(aPerformance) {
     let result = 0
@@ -35,6 +35,7 @@ export default function createStatementData(invoice, plays) {
     }
     return result
   }
+
   function totalAmount(data) {
     let result = 0
     for (let perf of data.performances) {
